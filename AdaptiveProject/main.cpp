583b056aa75b84a099b573d58447d377369d408a
Huajun Chai <chaihuajun@gmail.com>
2014-05-15 20:15:37 -0700

    Initial Commit

---
+++ AdaptiveProject/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  AdaptiveProject
+//
+//  Created by Huajun Chai on 5/15/14.
+//  Copyright (c) 2014 UC Davis. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
